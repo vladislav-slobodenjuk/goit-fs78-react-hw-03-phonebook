770f773709d19306c0af68c8f66cac874265f78c
vladislav-slobodenjuk <vladjun@gmail.com>
2023-07-01 23:17:24 +0300

    add life cicle

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,26 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+
+import { Container } from './App.styled';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
 import Filter from '../Filter/Filter';
-import { Container } from './App.styled';
+
+const STORAGE_KEY = 'contacts';
+
+const initContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-5', name: 'Vladislav Sl', number: '333-55-44' },
+];
+
+// localStorage.setItem(STORAGE_KEY, JSON.stringify(initContacts));
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: initContacts,
     filter: '',
   };
 
@@ -47,6 +55,17 @@ export class App extends Component {
       contacts: this.state.contacts.filter(contact => contact.id !== id),
     });
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedContacts) this.setState({ contacts: savedContacts });
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   render() {
     const { filter } = this.state;
